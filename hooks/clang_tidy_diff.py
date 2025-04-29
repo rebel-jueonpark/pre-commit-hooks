1d6ed3ac39794214713c664be02bf491fcef7c57
Jueon Park <jueonpark@rebellions.ai>
2025-04-29 13:05:46 +0900

    fix: remove python

--- hooks/clang_tidy_diff.py
+++ hooks/clang_tidy_diff.py
@@ -10,7 +10,7 @@ from hooks.utils import StaticAnalyzerCmd
 class ClangTidyDiffCmd(StaticAnalyzerCmd):
     """Class for the clang-tidy command."""
 
-    command = "python $REBEL_DEPENDENCY/llvm/share/clang/clang-tidy-diff.py"
+    command = "$REBEL_DEPENDENCY/llvm/share/clang/clang-tidy-diff.py"
     lookbehind = "LLVM version "
 
     def __init__(self, args: List[str]):
